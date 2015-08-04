02808e047c8cee31030e647d3e020e21092d79eb
Philip Lundrigan <philiplundrigan@gmail.com>
2015-08-04 00:53:59 -0400

    Add file encoding to top of file for python 2.7

--- gospellibrary/tests/test_catalog.py
+++ gospellibrary/tests/test_catalog.py
@@ -1,3 +1,5 @@
+# -*- coding: utf-8 -*-
+
 import unittest
 from gospellibrary import Catalog, Subitem, RelatedAudioItem, RelatedVideoItem, \
                           RelatedContentItem, NavItem, NavSection
